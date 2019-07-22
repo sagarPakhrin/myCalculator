102057f2f3726e5b55d3fd8297c5a0b4625657f1
sagar <sagarlama826@gmail.com>
2019-07-22 21:51:35 +0545

    calculations working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 var wrapper = document.querySelector(".wrapper").addEventListener("click", clickEvent);
 var isDecimal = false;
+var finished=false;
 
 function clickEvent(e){
 		var expression = document.querySelector('#expression');
 		var hist = document.querySelector('#hist');
+		if(finished){
+				expression.innerHTML = "";
+				hist.innerHTML = "";
+				finished= false;
+		}
 		if(e.target !== e.currentTarget){
 				var key = e.target;
 				var num = key.textContent;
@@ -25,9 +31,10 @@ function clickEvent(e){
 				}
 				else if(num == "="){
 						hist.innerHTML =expression.innerHTML;
-						expression.innerHTML = calculate(expression.innerHTML);
+						expression.innerHTML = eval(hist.innerHTML); 
+						finished = true;
 				}
-				else if(num == "+" || num == "-" || num == "*" || num == "X" ||    num == "/" ){
+				else if(num == "+" || num == "-" || num == "*" || num == "/" ){
 						isDecimal = false;
 						expression.innerHTML +=key.textContent;
 				}
@@ -35,51 +42,4 @@ function clickEvent(e){
 						expression.innerHTML +=key.textContent;
 				}
 		}
-}
-
-function calculate(expression){
-		var result = 0;
-		var exp=''; // to store operands
-		var postFix=[]; // this is to hold the postfix expression
-		var stack=[];// this is to hold the operator
-		for (var i = 0;i<expression.length; i++){
-				var character = expression[i];
-				if(!isNaN(character)){
-						exp +=character;
-						if(i==(expression.length-1)){
-								stack.push(exp);
-						}
-				}
-				else if(character == "+" ||character == "-" ||character == "X" ||character == "/"){
-						postFix.push(exp);
-						exp="";
-						while(stack.length !== 0 && (precedence(character)<=precedence(stack[stack.length-1]))){
-								postFix.push(stack.pop());
-						}
-						stack.push(character);
-				}
-		}
-		while(stack.length>0){
-				postFix.push(stack.pop());
-		}
-		console.log(postFix);
-		return result;
-}
-
-var precedence = function(operator){
-		switch(operator){
-				case "^":
-						return 3;
-				case "X":
-				case "/":
-						return 2;
-				case "+":
-				case "-":
-						return 1;
-				default:
-						return 0;
-		}
-}
-function stackTop(string){
-		return string[string.length -1];
 }
